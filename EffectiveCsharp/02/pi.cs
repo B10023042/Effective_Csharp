2c8593d203d62de4b7cc96c9163ed53692bd72da
Cloud <thirmoon@gmail.com>
2018-01-10 18:33:09 +0800

    add 02 folder

---
+++ EffectiveCsharp/02/pi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EffectiveCsharp._02
+{
+    class pi
+    {
+        private const decimal pi = 3.1415926m;
+    }
+}
